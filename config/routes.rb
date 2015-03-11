bd88340797b38301c382a28f2d68b3a2acee83eb
Kamari Aykes <aykeskc@gmail.com>
2015-03-11 14:01:44 -0400

    completes newsletter signup on postgres

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,12 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get '/login' => 'welcome#login' #(might not need)
+  post '/' => 'welcome#create'
   get '/locations' => 'locations#index'
   get '/locations/:id' => 'locations#show'
   get '/spotify/search' => 'spotify#search'
 
   resources :locations
   resources :spotify
-
   resources :emails, only: [:create]
 end
